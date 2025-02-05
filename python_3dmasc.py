83bee1f1ab3283ec287e5c3f8984bca4e2d75912
sophier <sophie.rothman1@gmail.com>
2025-02-05 14:56:14 +0100

    beginnings of a python file for running 3dmasc needs pauls help

---
+++ python_3dmasc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 14:50:25 2025
+
+@author: sophie.rothman
+"""
+
+import lidar_platform
+from lidar_platform import cc, las
+clouds = (pc1, pc2, core)  # pc1, pc2 and core are full paths to clouds 
+#I am assuming that pc1 is the total point cloud and pc2 is the tra 
+out = cc.q3dmasc(clouds, classifier)
